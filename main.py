3c9e5976b16fb03d3bd73be68b0f3b9a05eb970c
jan <jankompatscher@gmail.com>
2023-12-27 16:19:23 +0100

    tts

--- main.py
+++ main.py
@@ -3,12 +3,35 @@ import threading
 from video import run, stop
 from voice.recognize_commands import recognize_command
 import os
-import atexit
+import pyttsx3
 
 
 def cleanup():
-    if os.path.exists("dance/landmarks.csv"):
-        os.remove("dance/landmarks.csv")
+    if os.path.exists("dance/copy_landmarks.csv"):
+        os.remove("dance/copy_landmarks.csv")
+
+def read_aloud(engine, text):
+    engine.say(text)
+    engine.runAndWait()
+
+def text_to_speech(engine, text):
+    engine.setProperty('rate', 180)
+    engine.setProperty('volume', 0.9)
+
+    # Get all voices
+    voices = engine.getProperty('voices')
+
+    # Use an English voice
+    english_voices = [voice for voice in voices if "EN-US" in voice.id]
+    if english_voices:
+        engine.setProperty('voice', english_voices[-1].id)
+    else:
+        print("No English voice found")
+        
+    # Read label_txt aloud in a separate thread
+    tts_thread = threading.Thread(target=read_aloud, args=(engine, text,))
+    tts_thread.daemon = True
+    tts_thread.start()
 
 def listen_for_command():
     # Start the voice recognition in a separate thread
@@ -37,6 +60,10 @@ window.title("Little Dance Copiers")
 label_txt = "Welcome to Little Dance Copiers!\n\nPlease say 'dance' to start recording your dance moves.\nYou will get 5 moves.\nSay 'copy' to start copying the dance moves.";
 label = tk.Label(window, text=label_txt)
 label.pack(padx=10, pady=10)
+
+# Initialize the speech engine
+engine = pyttsx3.init()
+text_to_speech(engine, label_txt)
 
 # Create a response label
 response_label = tk.Label(window, text="")
