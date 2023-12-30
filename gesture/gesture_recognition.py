fe1f467e88aaa3e463e614451e46e48bdaeed0e1
jan <jankompatscher@gmail.com>
2023-12-30 12:56:17 +0100

    refactor code: made separate class for gesture recognition

---
+++ gesture/gesture_recognition.py
@@ -0,0 +1,109 @@
+import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
+import csv
+import numpy as np
+
+class GestureRecognition:
+    def __init__(self, csv_filename):
+        # initialize mediapipe
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose()
+        self.mp_drawing = mp.solutions.drawing_utils
+        # open csv files
+        self.file = open(csv_filename, 'w', newline='')
+        self.writer = csv.writer(self.file)
+        # write header row
+        self.landmark_names = [f'landmark_{i}_{k}' for i in range(33) for k in ['x', 'y', 'z', 'visibility']]
+        self.writer.writerow(self.landmark_names)
+        # variables
+        self.landmark_list = None
+        self.adjusted_pose_connections = None
+
+    def exclude_landmarks_and_connections(self, result):
+        # Exclude landmarks 0 to 10 and the respective connections
+        filtered_landmarks = [result.pose_landmarks.landmark[i] for i in range(11, 33)]
+        filtered_pose_connections = [connection for connection in self.mp_pose.POSE_CONNECTIONS
+                                     if connection[0] not in range(1, 11) and connection[1] not in range(1, 11)]
+        # Adjust the indices of the connections to account for the removed landmarks
+        adjusted_pose_connections = [(connection[0] - 11, connection[1] - 11) for connection in filtered_pose_connections]
+
+        # Create a new NormalizedLandmarkList and populate it with the filtered landmarks
+        landmark_list = landmark_pb2.NormalizedLandmarkList()
+        for landmark in filtered_landmarks:
+            landmark_list.landmark.add().CopyFrom(landmark)
+
+        return landmark_list, adjusted_pose_connections
+
+    def get_landmarks(self, rgb_image):
+        # Get the landmarks from the frame
+        results = self.pose.process(rgb_image)
+        if results.pose_landmarks:
+            self.landmark_list, self.adjusted_pose_connections = self.exclude_landmarks_and_connections(results)
+
+    def draw_landmarks(self, rgb_image, frame):
+        self.get_landmarks(rgb_image)
+
+        # Draw the landmarks with the adjusted connections
+        self.mp_drawing.draw_landmarks(frame, self.landmark_list, self.adjusted_pose_connections)
+
+        return frame
+
+    def center_and_scale(self, landmarks):
+        # Filter out landmarks 1 to 10
+        landmarks = [landmark for i, landmark in enumerate(landmarks) if i == 0 or i >= 11]
+        
+        # Calculate the average position
+        avg_x = sum(landmark.x for landmark in landmarks) / len(landmarks)
+        avg_y = sum(landmark.y for landmark in landmarks) / len(landmarks)
+        avg_z = sum(landmark.z for landmark in landmarks) / len(landmarks)
+
+        # Calculate the standard deviation
+        std_dev = np.std([(landmark.x, landmark.y, landmark.z) for landmark in landmarks])
+
+        # Subtract the average position from each landmark and divide by the standard deviation
+        scaled_landmarks = []
+        for landmark in landmarks:
+            scaled_landmark = type(landmark)()  # Create a new instance of the same class
+            scaled_landmark.x = (landmark.x - avg_x) / std_dev
+            scaled_landmark.y = (landmark.y - avg_y) / std_dev
+            scaled_landmark.z = (landmark.z - avg_z) / std_dev
+            scaled_landmark.visibility = landmark.visibility  # Keep the same visibility
+            scaled_landmarks.append(scaled_landmark)
+
+        return scaled_landmarks, avg_x, avg_y, avg_z, std_dev
+    
+    def dance(self):
+        # Save the landmarks to the CSV
+        scaled_landmarks, _, _, _, _ = self.center_and_scale(self.landmark_list.landmark)                 
+        landmark_row = [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in scaled_landmarks]
+        landmark_row = [item for sublist in landmark_row for item in sublist]
+        self.writer.writerow(landmark_row)
+
+        return scaled_landmarks
+
+    def copy(self, counter):
+        # also saved the copied landmarks to a csv file
+        scaled_landmarks = self.dance()
+        # Return the difference between the saved landmarks and the current ones
+        # Initialize saved_pose
+        saved_pose = None
+        with open('dance/dance_landmarks.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for i, row in enumerate(reader):
+                if i == counter:
+                    saved_pose = row
+                    break
+        
+        squared_diff = 0
+        # calculate the squared difference between the current pose and the saved pose                        
+        # Check if saved_pose is not None before calculating the squared difference
+        if saved_pose is not None:
+            for i, landmark in enumerate(scaled_landmarks):
+                squared_diff += (landmark.x - float(saved_pose[i*4]))**2 + (landmark.y - float(saved_pose[i*4+1]))**2 + (landmark.z - float(saved_pose[i*4+2]))**2
+            print(f"squared difference of row {counter}: {squared_diff}")
+
+        return squared_diff
+    
+    def cleanup(self):    
+        # Close the CSV file
+        self.file.close()
--- video.py
+++ video.py
@@ -6,50 +6,11 @@ import csv
 import numpy as np
 import ctypes
 import pygame.mixer
+from gesture.gesture_recognition import GestureRecognition
 
 # Global variable to control the video loop
 video_running = True
 window_name = 'Little Dance Copiers'
-
-def exclude_landmarks_and_connections(result, mp_pose):
-        # Exclude landmarks 0 to 10 and the respective connections
-        filtered_landmarks = [result.pose_landmarks.landmark[i] for i in range(11, 33)]
-        # Create a new NormalizedLandmarkList and populate it with the filtered landmarks
-        landmark_list = landmark_pb2.NormalizedLandmarkList()
-        for landmark in filtered_landmarks:
-            landmark_list.landmark.add().CopyFrom(landmark)
-
-        # Filter out connections that involve landmarks 1 to 10
-        filtered_pose_connections = [connection for connection in mp_pose.POSE_CONNECTIONS
-                                    if connection[0] not in range(1, 11) and connection[1] not in range(1, 11)]
-        # Adjust the indices of the connections to account for the removed landmarks
-        adjusted_pose_connections = [(connection[0] - 11, connection[1] - 11) for connection in filtered_pose_connections]
-
-        return landmark_list, adjusted_pose_connections
-
-def center_and_scale(landmarks):
-    # Filter out landmarks 1 to 10
-    landmarks = [landmark for i, landmark in enumerate(landmarks) if i == 0 or i >= 11]
-    
-    # Calculate the average position
-    avg_x = sum(landmark.x for landmark in landmarks) / len(landmarks)
-    avg_y = sum(landmark.y for landmark in landmarks) / len(landmarks)
-    avg_z = sum(landmark.z for landmark in landmarks) / len(landmarks)
-
-    # Calculate the standard deviation
-    std_dev = np.std([(landmark.x, landmark.y, landmark.z) for landmark in landmarks])
-
-    # Subtract the average position from each landmark and divide by the standard deviation
-    scaled_landmarks = []
-    for landmark in landmarks:
-        scaled_landmark = type(landmark)()  # Create a new instance of the same class
-        scaled_landmark.x = (landmark.x - avg_x) / std_dev
-        scaled_landmark.y = (landmark.y - avg_y) / std_dev
-        scaled_landmark.z = (landmark.z - avg_z) / std_dev
-        scaled_landmark.visibility = landmark.visibility  # Keep the same visibility
-        scaled_landmarks.append(scaled_landmark)
-
-    return scaled_landmarks, avg_x, avg_y, avg_z, std_dev
 
 def resize_window(window_name, cap):    # Get the size of the screen
     user32 = ctypes.windll.user32
@@ -71,8 +32,6 @@ def resize_window(window_name, cap):    # Get the size of the screen
     # Resize the window
     cv2.resizeWindow(window_name, new_width, screen_height)
 
-
-
 def draw_border(frame, color=(0, 255, 0), border_size=10):
     # Draw a colored border around the edges of the video
     frame[:border_size, :] = color
@@ -85,13 +44,6 @@ def run(mode, moves=5, bpm=60):
     video_running = True
     # Create a VideoCapture object
     cap = cv2.VideoCapture(0)
-
-    # Initialize MediaPipe Pose
-    mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose()
-
-    # Initialize drawing utility
-    mp_drawing = mp.solutions.drawing_utils
 
     # Check if camera opened successfully
     if not cap.isOpened(): 
@@ -113,15 +65,6 @@ def run(mode, moves=5, bpm=60):
     # Define the codec and create a VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # or use 'XVID'
     out = cv2.VideoWriter(f'dance/{mode}_output.mp4', fourcc, 20.0, (640, 480))
-
-    # Open the CSV file for writing
-    file = open(f'dance/{mode}_landmarks.csv', 'w', newline='')
-    writer = csv.writer(file)
-    
-    # Write the header row
-    landmark_names = [f'landmark_{i}_{k}' for i in range(33) for k in ['x', 'y', 'z', 'visibility']]
-    writer.writerow(landmark_names)
-
     
     ret, frame = cap.read()
     screenshots = []
@@ -131,6 +74,8 @@ def run(mode, moves=5, bpm=60):
     border_color = (0, 255, 0)
 
     delta_move = 60.0 / bpm
+
+    gr = GestureRecognition(csv_filename=f'dance/{mode}_landmarks.csv')
     
     while video_running:
         # Capture frame-by-frame
@@ -140,13 +85,8 @@ def run(mode, moves=5, bpm=60):
             # Convert the BGR image to RGB
             rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Process the image and draw poses on it
-            result = pose.process(rgb_image)
-        if result.pose_landmarks:
-            # Exclude landmarks 0 to 10 and the respective connections
-            landmark_list, adjusted_pose_connections = exclude_landmarks_and_connections(result, mp_pose)
-            # Draw the landmarks with the adjusted connections
-            mp_drawing.draw_landmarks(frame, landmark_list, adjusted_pose_connections)
+            # draw the landmarks
+            frame = gr.draw_landmarks(rgb_image, frame)
 
             # Update the counter every second
             if time.time() - start_time >= delta_move:
@@ -158,45 +98,24 @@ def run(mode, moves=5, bpm=60):
                     countdown_sound.play()
                     draw_border(frame, (255, 0, 0), 10)
 
-                if counter > 0 and counter <= moves and result.pose_landmarks:
+                if counter > 0 and counter <= moves:
                     screenshot = frame.copy()
                     screenshots.append(screenshot)
                     if mode == "dance":
-                        #write landmark x, y, z, visibility of all 33 landmarks to csv
-                        landmarks = [result.pose_landmarks.landmark[i] for i in range(33)]   
-                        scaled_landmarks, _, _, _, _ = center_and_scale(landmarks)                 
-                        landmark_row = [[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in scaled_landmarks]
-                        landmark_row = [item for sublist in landmark_row for item in sublist]
-                        writer.writerow(landmark_row)
+                        gr.dance()
 
                         correct_sound.play()
                     elif mode == "copy":
-                        #compare the current pose to the saved pose on row counter - 1
-                        # Initialize saved_pose
-                        saved_pose = None
-                        landmarks = [result.pose_landmarks.landmark[i] for i in range(33)]
-                        scaled_landmarks, _, _, _, _ = center_and_scale(landmarks)
-                        with open('dance/dance_landmarks.csv', newline='') as csvfile:
-                            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-                            for i, row in enumerate(reader):
-                                if i == counter:
-                                    saved_pose = row
-                                    break
-
-                        # calculate the squared difference between the current pose and the saved pose                        
-                        # Check if saved_pose is not None before calculating the squared difference
-                        if saved_pose is not None:
-                            squared_diff = 0
-                            for i, landmark in enumerate(scaled_landmarks):
-                                squared_diff += (landmark.x - float(saved_pose[i*4]))**2 + (landmark.y - float(saved_pose[i*4+1]))**2 + (landmark.z - float(saved_pose[i*4+2]))**2
-                            print(f"squared difference of row {counter}: {squared_diff}")
-                            # if the squared difference is greater than 0.5, draw a red border and play alow sound
-                            if squared_diff > 5:
-                                border_color = (0, 0, 255)
-                                error_sound.play()
-                            else:
-                                border_color = (0, 255, 0)
-                                correct_sound.play()
+                        #compare the current pose to the saved pose on row counter
+                        squared_diff = gr.copy(counter)
+                        
+                        # if the squared difference is greater than 0.5, draw a red border and play alow sound
+                        if squared_diff > 5:
+                            border_color = (0, 0, 255)
+                            error_sound.play()
+                        else:
+                            border_color = (0, 255, 0)
+                            correct_sound.play()
 
             if counter < moves:
                 # Draw the counter in the bottom right corner
@@ -231,14 +150,12 @@ def run(mode, moves=5, bpm=60):
                 video_running = False
         else:
             break
-    
-    # Close the CSV file
-    file.close()
 
     # When everything done, release the video capture and video write objects
     cap.release()
     out.release()
     cv2.destroyWindow(window_name)
+    gr.cleanup()
 
 def stop():
     global video_running
